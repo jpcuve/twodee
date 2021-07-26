52c421b62c008b8d772f32ee8b6253040c01bbe3
Jean-Pierre Cuvelliez <jpcuvelliez@gmail.com>
2021-07-26 15:06:59 +0200

    .

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include "TutorialApp.h"
-#include "asteroids/Asset.h"
 #include <glm/glm.hpp>
+#include "Asteroids.cpp"
 
 int main() {
     std::cout << "Hello, World! " << std::endl;
     glm::vec4 pos = glm::vec4(glm::vec3(0.0), 1.0);
     std::cout << pos.w << std::endl;
-    for (glm::vec2& p: asteroids::Asset::SHIP) {
-        std::cout << "(" << p.x << "," << p.y << ")" << std::endl;
-    }
+    aster::Game game;
+    for (auto item: game.items()) item->info();
     TutorialApp app;
     return app.run();
 }
